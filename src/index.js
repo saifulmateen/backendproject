f87aa837f4f84599fdfe2b73401f1ad0ee6d9824
Saif Ul Mateen <ulmateen@gmail.com>
2026-01-07 18:45:14 +0500

    Learned how to connect backend

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv"
+
+dotenv.config({
+    path: './.env'
+})
+import connectDB from "./db/index.js";
+
+
+connectDB()
+
+
+
+
+
+// import express from "express";
+
+// const app = express()
+// ;( async() => {
+//     try{
+//         await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("Error: ", error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Listening on port ${process.env.PORT}`);
+//         })
+//     } catch(error){
+//         console.log("Error: ", error);
+//         throw error;
+//     }
+// })()
+
