ad1f2686ac52fe16e6f9d68a6551bac740aeb2c8
Saif Ul Mateen <ulmateen@gmail.com>
2026-01-20 18:11:02 +0500

    We made a controller here

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,41 @@
+import { ApiError } from "../utils/ApiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {User} from "../models/user.models.js"
+import { uploadOnServer } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "First API by Saif ul Mateen"
+    const {userName, fullName, email, password} = req.body;
+    console.log("email: ", email);
+
+    if( [fullName, userName, email, password].some((field) => field?.trim() === "")){
+        throw new ApiError(400, "All fields are compulsory.")
+    }
+
+    const existingUser = User.findOne( {$or: [{userName}, {email}]} )
+
+    if(existingUser) throw new ApiError(409, "User already exists");
+
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    if(!avatarLocalPath) throw new ApiError(400, "Avatar required.");
+
+    const avatar = await uploadOnServer(avatarLocalPath)
+    const coverImage = await uploadOnServer(coverImageLocalPath)
+    
+    if(!avatar) throw new ApiError(400, "Avatar required.");
+
+    const user = await User.create({
+        fullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+        email,
+        userName: userName.toLowerCase()
     })
+
+    const createdUser = await User.findById(user._id).select("-passowrd -refreshToken")
 })
 
 export {registerUser}
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
-
+import {upload} from "../middlewares/multer.middleware.js"
+import { ApiError } from "../utils/ApiError.js";
 const router = Router()
 
+//we can transform this middleware in reusable middle ware by defining it in a constant
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount:1  
+        }
+    ]),
+    registerUser
+)
 
-router.route("/register").post(registerUser)
 
 export default router
